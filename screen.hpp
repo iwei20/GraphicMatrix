cc49d8e145f256ac9988f1c9faefa74df86253ac
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-01 18:12:20 -0500

    Take next edge when drawing

--- screen.hpp
+++ screen.hpp
@@ -39,7 +39,7 @@ class screen {
 
         void drawMatrix(edge_matrix& edges, std::tuple<short, short, short> color) {
             for(int i = 0; i < edges.width() - 1; ++i) {
-                drawLine({edges[1][i], edges[0][i]}, {edges[1][i], edges[0][i]}, color);
+                drawLine({edges[1][i], edges[0][i]}, {edges[1][i + 1], edges[0][i + 1]}, color);
             }
         }
 
