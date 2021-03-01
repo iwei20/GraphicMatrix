a1391d4a54088d8a5cf98d90c641b11728802f96
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-01 17:59:21 -0500

    Move add_point to private

--- edge_matrix.hpp
+++ edge_matrix.hpp
@@ -3,9 +3,10 @@
 #include "matrix.hpp"
 
 class edge_matrix : private matrix {
+    private:
+        void add_point(std::tuple<int, int, int>& point);
     public:
         edge_matrix();
 
-        void add_point(std::tuple<int, int, int>& point);
         void add_edge(std::tuple<int, int, int>& a, std::tuple<int, int, int>& b);
 };
