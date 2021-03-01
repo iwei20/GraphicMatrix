b8d22149942a0ef968467f37793020680954d147
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-01 18:13:30 -0500

    Add more tests

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "edge_matrix.hpp"
 #include "screen.hpp"
 #include <fstream>
+#include <iostream>
 
 int main() {
     std::ofstream fout("main.ppm");
     screen<512, 512> s;
     edge_matrix e;
-    e.add_edge({0, 0, 0}, {0, 512, 0});
+    e.add_edge({0, 0, 0}, {511, 511, 0});
+    s.drawMatrix(e, {255, 255, 255});
     fout << s;
+    std::cout << e;
     fout.close();
 }
