73de9fad44ac54b5acc9d8c03c23022adc6a59ff
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-05 13:56:05 -0500

    Create a slightly more interesting image

--- main.cpp
+++ main.cpp
@@ -4,18 +4,34 @@
 #include <fstream>
 #include <iostream>
 
+void rect(edge_matrix& e, int x, int y, int w, int h) {
+    e.add_edge({x, y, 0}, {x + w, y, 0});
+    e.add_edge({x, y, 0}, {x, y + h, 0});
+    e.add_edge({x + w, y + h, 0}, {x + w, y, 0});
+    e.add_edge({x + w, y + h, 0}, {x, y + h, 0});
+}
+
 int main() {
     std::ofstream fout("main.ppm");
     screen<512, 512> s;
     edge_matrix e;
-    e.add_edge({0, 0, 0}, {511, 256, 0});
+    for(int i = 0; i < 512; i += 512 / 8) {
+        e.add_edge({0, 0, 0}, {511, i, 0});
+    }
+    for(int i = 0; i < 512; i += 512 / 8) {
+        e.add_edge({0, 0, 0}, {i, 511, 0});
+    }
+    e.add_edge({0, 0, 0}, {511, 511, 0});
+
+    rect(e, 64, 64, 128, 128);
+    std::cout << "Testing add_edge: \n" << e;
     matrix ident(4, 4);
     for(int i = 0; i < 4; ++i) {
         ident[i][i] = 1;
     }
     e = dot(ident, e);
+    std::cout << "Testing identity dot product: \n" << e;
     s.drawMatrix(e, {255, 255, 255});
     fout << s;
-    std::cout << e;
     fout.close();
 }
