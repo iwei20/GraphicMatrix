9b9c9aa236825f493fbc3331c2c519b1d14eede8
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-05 13:57:55 -0500

    Switch everything to doubles

--- matrix.cpp
+++ matrix.cpp
@@ -1,10 +1,10 @@
 #include "matrix.hpp"
 
 #include <stdexcept>
-matrix::matrix(int rows, int cols) : data(rows, std::vector<int>(cols)) {}
+matrix::matrix(int rows, int cols) : data(rows, std::vector<double>(cols)) {}
 
 std::ostream& operator<<(std::ostream& out, matrix& mat) {
-    for(std::vector<int> row : mat.data) {
+    for(std::vector<double> row : mat.data) {
         for(int item : row) {
             out << item << " ";
         }
@@ -20,7 +20,7 @@ matrix dot(matrix& a, matrix& b) {
     matrix result(a.height(), b.width());
     for(int i = 0; i < a.height(); ++i) {
         for(int j = 0; j < b.width(); ++j) {
-            int sum = 0;
+            double sum = 0;
             for(int k = 0; k < a.width(); ++k) {
                 sum += a.data[i][k] * b.data[k][j];
             }
@@ -30,15 +30,15 @@ matrix dot(matrix& a, matrix& b) {
     return result;
 }
 
-std::vector<int>& matrix::operator[](int index) {
+std::vector<double>& matrix::operator[](int index) {
     return data[index];
 }
 
-int matrix::get(int row, int col) {
+double matrix::get(int row, int col) {
     return data[row][col];
 }
 
-void matrix::set(int row, int col, int num) {
+void matrix::set(int row, int col, double num) {
     data[row][col] = num;
 }
 
--- matrix.hpp
+++ matrix.hpp
@@ -6,14 +6,14 @@
 
 class matrix {
 protected:
-    std::vector<std::vector<int>> data;
+    std::vector<std::vector<double>> data;
 public:
     matrix(int rows, int cols);
     friend std::ostream& operator<<(std::ostream& out, matrix& mat);
     friend matrix dot(matrix& a, matrix& b);
-    std::vector<int>& operator[](int index);
-    int get(int row, int col);
-    void set(int row, int col, int num);
+    std::vector<double>& operator[](int index);
+    double get(int row, int col);
+    void set(int row, int col, double num);
 
     int width();
     int height();
