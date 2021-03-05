17e504ca4bca89cafb1771b33be8954f5dd94108
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-05 14:31:02 -0500

    Change header as well

--- matrix.hpp
+++ matrix.hpp
@@ -10,7 +10,7 @@ protected:
 public:
     matrix(int rows, int cols);
     friend std::ostream& operator<<(std::ostream& out, matrix& mat);
-    friend matrix dot(matrix& a, matrix& b);
+    friend matrix operator*(matrix& a, matrix& b);
     std::vector<double>& operator[](int index);
     double get(int row, int col);
     void set(int row, int col, double num);
