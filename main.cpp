9633733d33f138c3503fdf4cfc147b106777a9d4
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-03-08 05:06:42 -0500

    Make a slightly more interesting image

--- main.cpp
+++ main.cpp
@@ -15,15 +15,14 @@ int main() {
     std::ofstream fout("main.ppm");
     screen<512, 512> s;
     edge_matrix e;
-    for(double i = 0; i < 512; i += 512 / 8) {
+    for(double i = 511; i >= 1; i /= 1.2) {
         e.add_edge({0, 0, 0}, {511, i, 0});
     }
-    for(double i = 0; i < 512; i += 512 / 8) {
+    for(double i = 511; i >= 1; i /= 1.2) {
         e.add_edge({0, 0, 0}, {i, 511, 0});
     }
     e.add_edge({0, 0, 0}, {511, 510.5, 0});
 
-    rect(e, 64, 64, 128, 128);
     std::cout << "Testing add_edge: \n" << e;
     matrix ident(4, 4);
     for(int i = 0; i < 4; ++i) {
